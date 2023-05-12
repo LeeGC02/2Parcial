d43362de13e264aedc0b55f2d182d865b4a89ab8
LeeGC02 <nanna.lia.g.c@gmail.com>
2023-05-12 11:38:54 -0400

    Primer Componente

---
+++ src/components/CardsMS.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./CardsMS.css";
+const CardsMS = () => {
+  return (
+    <div className="container-card">
+      <div className="title"></div>
+    </div>
+  );
+};
+
+export default CardsMS;
